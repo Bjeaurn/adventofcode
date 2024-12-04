23d7218abbc6da2b08e801f892057757efb2b361
Bjorn 'Bjeaurn <bjorn@somnu.com>
2024-12-04 13:30:05 +0100

    feat(2024): done day4 part 1

--- aoc2024/src/day4.rs
+++ aoc2024/src/day4.rs
@@ -5,11 +5,17 @@ pub fn main() {
     let data: String = load_file(file_name).unwrap();
     let map = create_map(data);
     let mut count = 0;
-    &map.clone().into_iter().enumerate()
-        .for_each(|(i, v)| {
-             v.into_iter().enumerate().for_each(|(j, _)| 
-            count += find_xmas(j, i, &map)
-        )});
+    &map.clone().into_iter().enumerate().for_each(|(y, v)| {
+        v.into_iter().enumerate().for_each(|(x, _)| {
+            // Part 1
+            // let add = find_xmas(x, y, &map);
+            // count += add;
+
+            // Part 2
+            let add = find_x_mas(x, y, &map);
+            count += add;
+        });
+    });
     println!("{count}");
 }
 
@@ -22,48 +28,140 @@ fn create_map(data: String) -> Vec<Vec<String>> {
     return map;
 }
 
-fn find_xmas(x: isize, y: isize, map: &Vec<Vec<String>>) -> i32 {
+fn find_x_mas(x: usize, y: usize, map: &Vec<Vec<String>>) -> i32 {
     let mut count = 0;
-    if map[y][x] == "X" {
-        if find_right(x, y, &map) { count += 1; }
-        if find_left(x, y, &map) { count += 1; }
-        if find_up(x, y, &map) { count += 1; }
-        if find_down(x, y, &map) { count += 1; }
+    if (map[y][x] == "A") {
+        println!("A found");
     }
     return count;
 }
 
-fn find_right(x: isize, y: isize, map: &Vec<Vec<String>>) -> bool {
-    println!("f_right: {x}, {y}");
-    if x + 3 > map[y].len() - 1 { return false; }
+fn find_xmas(x: usize, y: usize, map: &Vec<Vec<String>>) -> i32 {
+    let mut count = 0;
+    if map[y][x] == "X" {
+        if find_right(y, x, &map) {
+            count += 1;
+        }
+        if find_left(y, x, &map) {
+            count += 1;
+        }
+        if find_up(y, x, &map) {
+            count += 1;
+        }
+        if find_down(y, x, &map) {
+            count += 1;
+        }
+        if find_dianogal_dl(y, x, map) {
+            count += 1;
+        }
+        if find_dianogal_dr(y, x, map) {
+            count += 1;
+        }
+        if find_dianogal_ul(y, x, map) {
+            count += 1;
+        }
+        if find_dianogal_ur(y, x, map) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn find_right(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    if x + 3 > map[y].len() - 1 {
+        return false;
+    }
     if map[y][x + 3] == "S" && map[y][x + 2] == "A" && map[y][x + 1] == "M" {
-        return true
+        return true;
     }
     return false;
 }
 
-fn find_left(x: isize, y: isize, map: &Vec<Vec<String>>) -> bool {
-    println!("f_left: {x}, {y}");
-    if (x - 3) < 0 { return false; }
+fn find_left(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    // println!("f_left: {x}, {y}");
+    if x.checked_sub(3).is_none() {
+        return false;
+    }
     if map[y][x - 3] == "S" && map[y][x - 2] == "A" && map[y][x - 1] == "M" {
-        return true
+        return true;
     }
     return false;
 }
-fn find_up(y: isize, x: isize, map: &Vec<Vec<String>>) -> bool {
-    println!("f_up: {x}, {y}");
-    if (y - 3) < 0 { return false;}
-    if map[y - 3][x] == "S" && map[y - 2][x] == "A" && map[y - 1][x] == "M" {
-        return true
+fn find_up(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    if y.checked_sub(3).is_none() {
+        return false;
+    }
+    if map.get(y - 3).unwrap().get(x).unwrap() == "S"
+        && map[y - 2][x] == "A"
+        && map[y - 1][x] == "M"
+    {
+        return true;
     }
     return false;
 }
 
-fn find_down(y: isize, x: isize, map: &Vec<Vec<String>>) -> bool {
-    println!("f_down: {x}, {y}");
-    if y + 3 > isize::from(map.len()) - 1 { return false; }
-    if map[y + 3][x] == "S" && map[y + 2][x] == "A" && map[y + 1][x] == "M" {
-        return true
+fn find_down(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    if y + 3 > map.len() - 1 {
+        return false;
+    }
+
+    if map.get(y + 3).unwrap().get(x).unwrap() == "S"
+        && map[y + 2][x] == "A"
+        && map[y + 1][x] == "M"
+    {
+        return true;
+    }
+    return false;
+}
+
+fn find_dianogal_ur(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    if y.checked_sub(3).is_none() || x + 3 > map[y].len() - 1 {
+        return false;
+    }
+    if map.get(y - 3).unwrap().get(x + 3).unwrap() == "S"
+        && map[y - 2][x + 2] == "A"
+        && map[y - 1][x + 1] == "M"
+    {
+        return true;
+    }
+    return false;
+}
+
+fn find_dianogal_dr(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    if y + 3 > map.len() - 1 || x + 3 > map[y].len() - 1 {
+        return false;
+    }
+    if map.get(y + 3).unwrap().get(x + 3).unwrap() == "S"
+        && map[y + 2][x + 2] == "A"
+        && map[y + 1][x + 1] == "M"
+    {
+        return true;
+    }
+    return false;
+}
+
+fn find_dianogal_ul(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    if y.checked_sub(3).is_none() || x.checked_sub(3).is_none() {
+        return false;
+    }
+    if map.get(y - 3).unwrap().get(x - 3).unwrap() == "S"
+        && map[y - 2][x - 2] == "A"
+        && map[y - 1][x - 1] == "M"
+    {
+        return true;
+    }
+    return false;
+}
+
+fn find_dianogal_dl(y: usize, x: usize, map: &Vec<Vec<String>>) -> bool {
+    if y + 3 > map.len() - 1 || x.checked_sub(3).is_none() {
+        return false;
+    }
+    if map.get(y + 3).unwrap().get(x - 3).unwrap() == "S"
+        && map[y + 2][x - 2] == "A"
+        && map[y + 1][x - 1] == "M"
+    {
+        return true;
     }
     return false;
 }
